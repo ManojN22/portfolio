bdd95b39d4021331dfd915be1ce26c942c865822
ManojN22 <manojkumar22072001@gmail.com>
2024-07-29 10:15:02 -0400

    update

--- app/Projects/projects.ts
+++ app/Projects/projects.ts
@@ -6,17 +6,24 @@ const projects = [
           image: '/portfolio/project/p4.png',
           },
           {
-          title: 'SAM Scheduler',
-          description: 'SAM Scheduler is an enhancement to the Completely Fair Scheduler (CFS) used in Linux. By integrating an interactivity scoring mechanism inspired by the ULE scheduler, SAM aims to improve the interactivity and performance of both single-threaded and multi-threaded applications.',
-          image: '/portfolio/project/p2.svg',
-          github: 'https://github.com/ManojN22/SAM-Enhancing-the-responsiveness-of-the-Linux-CFS-scheduler-C',
-          },
-          {
-            title: 'ReDoS Fuzz: Regex Denial of Service in JavaScript',
-            description: `Developed a gray box fuzzer to induce ReDoS attacks by exploiting the backtracking feature of the JavaScript regex engine,
-adding hooks for regex coverage and generating input based on that coverage.`,
-            image: '/portfolio/project/p5.png',
-          },
+            title: 'Satellite Image Analysis using YOLOV3 -Angular, Flask, MongoDB',
+            description: 'This project analyze satellite images for specific structures like Radomes and Mesh Antennas using YOLOV3. The system comprises a Flask-based backend for processing and a modern Angular frontend for visualization and interaction.',
+            image: '/portfolio/project/p6.png',
+            github: 'https://github.com/ManojN22/dome-and-antenna-detection',
+            },
+  
+          // {
+          // title: 'SAM Scheduler',
+          // description: 'SAM Scheduler is an enhancement to the Completely Fair Scheduler (CFS) used in Linux. By integrating an interactivity scoring mechanism inspired by the ULE scheduler, SAM aims to improve the interactivity and performance of both single-threaded and multi-threaded applications.',
+          // image: '/portfolio/project/p2.svg',
+          // github: 'https://github.com/ManojN22/SAM-Enhancing-the-responsiveness-of-the-Linux-CFS-scheduler-C',
+          // },
+//           {
+//             title: 'ReDoS Fuzz: Regex Denial of Service in JavaScript',
+//             description: `Developed a gray box fuzzer to induce ReDoS attacks by exploiting the backtracking feature of the JavaScript regex engine,
+// adding hooks for regex coverage and generating input based on that coverage.`,
+//             image: '/portfolio/project/p5.png',
+//           },
           {
           title: 'Sign-Up Form',
           description: 'This project features a responsive sign-up form created using HTML, CSS, and JavaScript. The form is designed to be user-friendly and adapts to various screen sizes, ensuring a seamless experience on both desktop and mobile devices.',
